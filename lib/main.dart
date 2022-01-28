ed8ed99edfd8a03f7f6b4f43d91ea263acda0f41
Nij Mehar <nijmehar16@gmail.com>
2022-01-28 13:54:07 +0530

    ui second day

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
-// ignore_for_file: avoid_unnecessary_containers
+// ignore_for_file: avoid_unnecessary_containers, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:test_project/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: const Text("welcome"),
-          ),
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
