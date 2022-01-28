07c4f03891f4ef477344aee44676b2f78ae75435
Nij Mehar <nijmehar16@gmail.com>
2022-01-28 19:57:08 +0530

    day 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: avoid_unnecessary_containers, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:test_project/pages/home_page.dart';
 // import 'package:test_project/pages/home_page.dart';
 import 'package:test_project/pages/login_page.dart';
+import 'package:test_project/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,13 +19,17 @@ class MyApp extends StatelessWidget {
     bringVegetable(2);
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.indigo),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        fontFamily: GoogleFonts.pacifico().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      // initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
-        "/home":(context) => Homepage(),
-        "/login" :(context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => Homepage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,25 +1,72 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:test_project/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("login"),
-      ),
-      // ignore: avoid_unnecessary_containers
-      body: Container(
-        child: Center(
-          child: Text(
-            "login",
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.blue,
-              fontWeight: FontWeight.bold
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppBar(
+              title: Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
             ),
-          ),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/loginImage.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "User name",
+                  labelText: "username",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "password",
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              child: Text("login"),
+              style: TextButton.styleFrom(
+                minimumSize: Size(120, 30),
+              ),
+              onPressed: () {
+                print("navigated to home page");
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+              },
+            )
+          ],
         ),
       ),
     );
